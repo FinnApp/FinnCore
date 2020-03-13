8876f91be5808001aef93565ba419ff892305993
Maxim Ivanov <maxim11853@gmail.com>
2020-03-13 14:10:40 +0200

    feat: add API for Category

--- src/Category.cpp
+++ src/Category.cpp
@@ -1,3 +1,51 @@
 #include "Category.hpp"
 
-Category::Category(Id id) : UniqueEntity{id} {}
+#include <algorithm>
+#include <cassert>
+
+Category::InvalidSubcategory::InvalidSubcategory(Id categoryId) :
+    std::runtime_error{"Subcategory with ID " + std::to_string(categoryId) + " not found"}
+{
+}
+
+Category::Category(Id id, const std::string& name) : UniqueEntity{id}, NamedEntity{name} {}
+
+void Category::addSubcategory(const std::shared_ptr<Category>& subcategory)
+{
+    assert(subcategory);
+
+    subcategory->setParentCategory(shared_from_this());
+    subcategories_.emplace_back(subcategory);
+}
+
+void Category::removeSubcategoryBy(Id subcategoryId)
+{
+    subcategories_.erase(std::remove_if(subcategories_.begin(), subcategories_.end(), IdPredicate{subcategoryId}),
+                         subcategories_.end());
+}
+
+Category& Category::subcategoryBy(Id subcategoryId)
+{
+    auto it = std::find_if(subcategories_.begin(), subcategories_.end(), IdPredicate{subcategoryId});
+
+    if (it == subcategories_.end()) throw InvalidSubcategory{subcategoryId};
+
+    return **it;
+}
+
+size_t Category::subcategoriesCount() const
+{
+    return subcategories_.size();
+}
+
+std::weak_ptr<Category> Category::parentCategory() const
+{
+    return parentCategory_;
+}
+
+void Category::setParentCategory(std::shared_ptr<Category>&& parentCategory)
+{
+    assert(parentCategory);
+
+    parentCategory_ = std::move(parentCategory);
+}
--- src/Category.hpp
+++ src/Category.hpp
@@ -1,6 +1,10 @@
 #pragma once
 
+#include "NamedEntity.hpp"
 #include "UniqueEntity.hpp"
+
+#include <memory>
+#include <vector>
 
 /*!
  * \brief The Category class
@@ -8,11 +12,65 @@
  * Represents the transaction category and has tree structure:
  * category has subcategories which also can have subsubcategories and so on.
  */
-class Category : public UniqueEntity<Category>
+class Category : public UniqueEntity<Category>, public NamedEntity, public std::enable_shared_from_this<Category>
 {
 public:
     /*!
-     * \brief Constructor
+     * \brief Exception is thrown if subcategory with some ID is not found
      */
-    Category(Id id);
+    struct InvalidSubcategory : std::runtime_error  // TODO rename to *NotFound
+    {
+        /*!
+         * \param categoryId is used in exception message
+         */
+        InvalidSubcategory(Id categoryId);
+    };
+
+    /*!
+     * \brief Constructor
+     *
+     * Constructs category object with \p id and \p name
+     *
+     * \param id Unique non-negative integer that represents category ID
+     * \param name Unicode string that represents category name
+     * \exception SameIdError is thrown if category with \p id already exists
+     * \exception EmptyNameError is thrown if \p name is empty
+     */
+    Category(Id id, const std::string& name);
+
+    /*!
+     * \brief Adds subcategory to account
+     *
+     * Subcateogry is owned by parent category. Subcategory has reference to its parent.
+     */
+    void addSubcategory(const std::shared_ptr<Category>& subcategory);
+
+    /*!
+     * \brief Removes subcategory with \p subcategoryId from parent category
+     */
+    void removeSubcategoryBy(Id subcategoryId);
+
+    /*!
+     * \brief Retrives the subcategory with \p subcategoryId from category
+     * \return Reference to existing subcategory
+     * \exception InvalidSubcategory is thrown if subcategory with \p subcategoryId isn't found in the parent category
+     */
+    Category& subcategoryBy(Id subcategoryId);
+
+    /*!
+     * \return Number of created subcategories in the Category
+     */
+    size_t subcategoriesCount() const;
+
+    /*!
+     * \return Weak pointer to parent category. It can be checked to find if parent category is alive
+     */
+    std::weak_ptr<Category> parentCategory() const;
+
+private:
+    void setParentCategory(std::shared_ptr<Category>&& parentCategory);
+
+private:
+    std::weak_ptr<Category> parentCategory_;
+    std::vector<std::shared_ptr<Category>> subcategories_;
 };
