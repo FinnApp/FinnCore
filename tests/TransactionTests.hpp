95e818c1301ad9ee8e3d8a344d2738154e36c7a8
Maxim Ivanov <maxim11853@gmail.com>
2020-04-15 15:25:30 +0300

    test: add help utils to create Transaction in tests

--- tests/TransactionTests.hpp
+++ tests/TransactionTests.hpp
@@ -2,11 +2,22 @@
 
 #include "Utils.hpp"
 
+#include "CategoryTests.hpp"
 #include "Transaction.hpp"
+#include "WalletTests.hpp"
+
+#include <boost/date_time/date.hpp>
 
 const double DefaultAmount = 5.0;
+const boost::posix_time::ptime DefaultCreateionDate{boost::gregorian::date{2020, 11, 26}};
+
+inline std::shared_ptr<Transaction> createTransaction(std::weak_ptr<Wallet> wallet, std::weak_ptr<Category> category)
+{
+    return std::make_shared<Transaction>(DefaultId, DefaultAmount, wallet, category, DefaultCreateionDate);
+}
 
 inline std::shared_ptr<Transaction> createTransaction()
 {
-    return std::make_shared<Transaction>(DefaultId, DefaultAmount);
+    return std::make_shared<Transaction>(
+        DefaultId, DefaultAmount, createWallet(), createCategory(), DefaultCreateionDate);
 }
